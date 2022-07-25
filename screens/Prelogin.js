95739900ddf2e8ef004d43277d5567e4c3050406
octorose <allouchhatim@gmail.com>
2022-07-25 12:27:02 +0100

    init

---
+++ screens/Prelogin.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Prelogin = () => {
+  return (
+    <View >
+      <Text className='text-red-500'>Prelogin</Text>
+    </View>
+  )
+}
+
+export default Prelogin
