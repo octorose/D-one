d61db3181268709d654450026a79782e16d96e4a
octorose <allouchhatim@gmail.com>
2022-08-17 08:45:27 +0100

    filling stats

--- components/Stats.js
+++ components/Stats.js
@@ -3,6 +3,8 @@ import react, { Component, useEffect } from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Daily from '../screens/Daily';
 import Average from '../screens/Average';
+import ReactNativeCalendarStrip from 'react-native-calendar-strip';
+import moment from 'moment';
 const Tab = createMaterialTopTabNavigator();
 
 const Stats = () => { 
@@ -26,49 +28,54 @@ const Stats = () => {
         useEffect(() => {
             console.log(currentTabIndex);
           }, [currentTabIndex]);
-        return (
-    <View style={{height:"70%",width:'98%', marginLeft:'auto', marginRight:'auto'}}>
-      <View style={{flex:1}}>
-        <View style={{}}>
-            <View style={{flexDirection:'row', marginTop:30, marginBottom:30, position:'relative', height:39}}>
-      
-                <TouchableOpacity className='justify-center items-left' style={{ height:'100%', backgroundColor:'#fff', borderRadius:18, borderWidth:1,borderBottomWidth:-1,borderBottomRightRadius:0, borderTopRightRadius:0, borderBottomLeftRadius:0,shadowColor: "#000000",
-                shadowOpacity: 0.3,
-                shadowRadius: 2,
-                shadowOffset: {
-                  height: -1,
-                  width: 2,
-                }, borderColor:'#e6e6e6',flex:1,zIndex:2}} onPressIn={UpdateCurrentTabIndex0}>
-                    <Text className='font-bold text-lg text-left ml-3'>
-                    Daily
-                    </Text>
-                </TouchableOpacity>
-                <TouchableOpacity className='justify-center items-left'style={[styles.up, currentTabIndex == 1 && {  shadowOffset: {
-                  height: -2,
-                  width: -1,
-                  
-                },zIndex:4}]}onPressIn={UpdateCurrentTabIndex1}>
-                    <Text className='font-bold text-lg ml-3'>
-                    Average
-                    </Text>
-                </TouchableOpacity>
-            
-            </View>
-        </View>
-      </View>
-      <View style={{width:'99.70%',height:'100%', backgroundColor:'#fff',top:40,shadowColor: "#00",
-                shadowOpacity: 0.3,
-                shadowRadius: 2,
-                shadowOffset: {
-                  height: 3,
-                  width: 2,
-                },zIndex:-3,borderTopWidth:-1,borderBottomLeftRadius:18,borderBottomRightRadius:18}}>
 
-      </View>
-    </View>
- 
-    )
-              
+        return (
+          <View style={{height:"73%",width:'98%', marginLeft:'auto', marginRight:'auto'}}>
+            <View style={{flex:1}}>
+              <View>
+                  <View style={{flexDirection:'row', marginTop:30, marginBottom:30, position:'relative', height:44}}>
+            
+                      <TouchableOpacity className='justify-center items-left' style={{ height:'100%', backgroundColor:'#fff', borderRadius:18, borderWidth:1,borderBottomWidth:-1,borderBottomRightRadius:0, borderTopRightRadius:0, borderBottomLeftRadius:0,shadowColor: "#000000",
+                      shadowOpacity: 0.3,
+                      shadowRadius: 2,
+                      shadowOffset: {
+                        height: -1,
+                        width: 2,
+                      }, borderColor:'#e6e6e6',flex:1,zIndex:2}} onPressIn={UpdateCurrentTabIndex0}>
+                          <Text className='font-bold text-lg text-left ml-3'>
+                          Daily
+                          </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity className='justify-center items-left'style={[styles.up, currentTabIndex == 1 && {  shadowOffset: {
+                        height: -2,
+                        width: -1,
+                        
+                      },zIndex:4}]}onPressIn={UpdateCurrentTabIndex1}>
+                          <Text className='font-bold text-lg ml-3'>
+                          Average
+                          </Text>
+                      </TouchableOpacity>
+                  
+                  </View>
+              </View>
+            </View>
+            <View className='justify-center items-center' style={{width:'99.70%',height:'100%', backgroundColor:'#fff',top:40,shadowColor: "#00",
+                      shadowOpacity: 0.3,
+                      shadowRadius: 2,
+                      shadowOffset: {
+                        height: 3,
+                        width: 2,
+                      },zIndex:-3,borderTopWidth:-1,borderBottomLeftRadius:18,borderBottomRightRadius:18,}}>
+                                <ReactNativeCalendarStrip daySelectionAnimation={{highlightColor:'#2983f5',type:'background'}} maxDate={moment()} scrollable={true} style={{width:'95%', height:'15%', marginTop:"42%"}}/>
+                        {currentTabIndex == 1 ? 
+                        <Average/> 
+                        : 
+                        <Daily/>
+                        }
+            </View>
+          </View>
+
+    );        
 }
 
 export default Stats;
--- screens/Daily.js
+++ screens/Daily.js
@@ -1,11 +1,20 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
+import ReactNativeCalendarStrip from 'react-native-calendar-strip'
 
 export class Daily extends Component {
   render() {
     return (
       <View>
-        <Text>Daily</Text>
+      <View style={{flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start', }}>
+        <View className = 'm-2 mt-2 rounded-xl' style={{width:'45%', height:'30%', backgroundColor:'#6ccff8'}}></View>
+        <View className = 'm-2 mt-2 rounded-xl' style={{width:'45%', height:'30%', backgroundColor:'#9599f7'}}></View>
+        <View className = 'bg-gray-200  m-2 rounded-xl' style={{width:'45%', height:'30%', backgroundColor:'#ee8f91'}}></View>
+        <View className = 'bg-gray-200  m-2 rounded-xl' style={{width:'45%', height:'30%', backgroundColor:'#aef5c1'}}></View>
+      </View>
       </View>
     )
   }
